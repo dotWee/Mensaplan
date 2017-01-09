029d44abe6ef54bcff7e9b57f747dfe6821ff501
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2017-01-09 16:31:29 +0100

    Add null-check for the menu item class

--- app/src/main/java/de/dotwee/rgb/canteen/view/activities/MainActivity.java
+++ app/src/main/java/de/dotwee/rgb/canteen/view/activities/MainActivity.java
@@ -3,6 +3,7 @@ package de.dotwee.rgb.canteen.view.activities;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
@@ -111,7 +112,10 @@ public class MainActivity extends AppCompatActivity implements Spinner.OnItemSel
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(@Nullable MenuItem item) {
+        if (item == null) {
+            return false;
+        }
 
         switch (item.getItemId()) {
 
