3cf3054ede77c8c9c43b365209c1ec7438617505
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2017-01-15 15:59:28 +0100

    Define a basic empty activity with layout

---
+++ app/src/main/java/de/dotwee/rgb/canteen/view/activities/CacheActivity.java
@@ -0,0 +1,38 @@
+package de.dotwee.rgb.canteen.view.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import de.dotwee.rgb.canteen.R;
+import timber.log.Timber;
+
+/**
+ * Created by lukas on 15.01.17.
+ */
+public class CacheActivity extends AppCompatActivity {
+    private static final String TAG = CacheActivity.class.getSimpleName();
+
+    @BindView(R.id.coordinatorLayout)
+    CoordinatorLayout coordinatorLayout;
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @BindView(R.id.recyclerView)
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Timber.tag(TAG);
+
+        setContentView(R.layout.activity_cache);
+        ButterKnife.bind(this);
+    }
+}
