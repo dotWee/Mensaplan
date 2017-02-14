868584277d497df91b653cf312cf4ab57fb57512
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2017-02-14 09:45:49 +0100

    Implement a custom timber tree to handle exceptions handled by timber

--- app/src/main/java/de/dotwee/rgb/canteen/CanteenApplication.java
+++ app/src/main/java/de/dotwee/rgb/canteen/CanteenApplication.java
@@ -2,12 +2,15 @@ package de.dotwee.rgb.canteen;
 
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
 import android.support.multidex.MultiDexApplication;
+import android.util.Log;
 
 import com.crashlytics.android.Crashlytics;
 import com.crashlytics.android.core.CrashlyticsCore;
 
 import io.fabric.sdk.android.Fabric;
+import timber.log.Timber;
 
 /**
  * Created by lukas on 10.11.2016.
@@ -24,7 +27,11 @@ public class CanteenApplication extends MultiDexApplication {
     public void onCreate() {
         super.onCreate();
 
+        // Init Fabric
         initFabric();
+
+        // Init Timber
+        initTimber();
 
         // Init static preferences
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
@@ -40,4 +47,37 @@ public class CanteenApplication extends MultiDexApplication {
         Fabric.with(this, new Crashlytics.Builder().core(core).build());
     }
 
+
+    private void initTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        } else {
+            Timber.plant(new CrashlyticsTree());
+        }
+    }
+
+    public static class CrashlyticsTree extends Timber.Tree {
+        private static final String TAG = CrashlyticsTree.class.getSimpleName();
+
+        private static final String CRASHLYTICS_KEY_PRIORITY = "priority";
+        private static final String CRASHLYTICS_KEY_TAG = "tag";
+        private static final String CRASHLYTICS_KEY_MESSAGE = "message";
+
+        @Override
+        protected void log(int priority, @Nullable String tag, @Nullable String message, @Nullable Throwable t) {
+            if (priority == Log.VERBOSE || priority == Log.DEBUG || priority == Log.INFO) {
+                return;
+            }
+
+            Crashlytics.setInt(CRASHLYTICS_KEY_PRIORITY, priority);
+            Crashlytics.setString(CRASHLYTICS_KEY_TAG, tag);
+            Crashlytics.setString(CRASHLYTICS_KEY_MESSAGE, message);
+
+            if (t == null) {
+                Crashlytics.logException(new Exception(message));
+            } else {
+                Crashlytics.logException(t);
+            }
+        }
+    }
 }
