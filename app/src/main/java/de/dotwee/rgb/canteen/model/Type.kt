fff56a6d2a975d0c7b5d78043d206d73e02ee31f
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2017-12-25 02:51:36 +0100

    Define indicator values within resources

--- app/src/main/java/de/dotwee/rgb/canteen/model/Type.kt
+++ app/src/main/java/de/dotwee/rgb/canteen/model/Type.kt
@@ -18,12 +18,14 @@ enum class Type private constructor(@param:StringRes @field:StringRes
                                     @get:ColorRes
                                     val colorId: Int, @param:ColorRes @field:ColorRes
                                     @get:ColorRes
-                                    val darkColorId: Int) {
+                                    val darkColorId: Int,
+                                    @param:StringRes @get:StringRes
+                                    val resId: Int) {
 
-    SOUP(R.string.type_soup, R.color.md_indigo_600, R.color.md_indigo_800),
-    MAIN(R.string.type_main_dish, R.color.md_blue_600, R.color.md_blue_800),
-    SIDE_DISH(R.string.type_side_dish, R.color.md_teal_600, R.color.md_teal_800),
-    DESSERT(R.string.type_dessert, R.color.md_green_600, R.color.md_green_800),
-    NONE(R.string.type_none, R.color.md_grey_600, R.color.md_grey_800)
+    SOUP(R.string.type_soup, R.color.md_indigo_600, R.color.md_indigo_800, R.string.type_indicator_soup),
+    MAIN(R.string.type_main_dish, R.color.md_blue_600, R.color.md_blue_800, R.string.type_indicator_main),
+    SIDE_DISH(R.string.type_side_dish, R.color.md_teal_600, R.color.md_teal_800, R.string.type_indicator_side_dish),
+    DESSERT(R.string.type_dessert, R.color.md_green_600, R.color.md_green_800, R.string.type_indicator_dessert),
+    NONE(R.string.type_none, R.color.md_grey_600, R.color.md_grey_800, R.string.type_indicator_none)
 
 }
