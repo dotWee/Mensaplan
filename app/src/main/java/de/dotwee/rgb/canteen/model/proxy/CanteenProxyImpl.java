308b0c16db91778f5afd3a79f6408defba865b09
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2018-04-18 20:50:02 +0200

    Allow caching requests using cache property for OkHttpClient

--- app/src/main/java/de/dotwee/rgb/canteen/model/proxy/CanteenProxyImpl.java
+++ app/src/main/java/de/dotwee/rgb/canteen/model/proxy/CanteenProxyImpl.java
@@ -1,10 +1,13 @@
 package de.dotwee.rgb.canteen.model.proxy;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
+import java.io.File;
 import java.util.Locale;
 
 import de.dotwee.rgb.canteen.model.Location;
+import okhttp3.Cache;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 
@@ -13,10 +16,14 @@ import okhttp3.Request;
  */
 
 public class CanteenProxyImpl implements CanteenProxy {
+    private static int CACHE_SIZE = 50 * 1024 * 1024; // 50 MiB
+
     private OkHttpClient okHttpClient;
+    private File cacheDirectory;
+    private Cache cache;
 
-    public CanteenProxyImpl() {
-
+    public CanteenProxyImpl(@Nullable File cacheDirectory) {
+        this.cacheDirectory = cacheDirectory;
     }
 
 
@@ -24,7 +31,17 @@ public class CanteenProxyImpl implements CanteenProxy {
     @Override
     public OkHttpClient getHttpClient() {
         if (okHttpClient == null) {
-            okHttpClient = new OkHttpClient();
+            if (cacheDirectory != null) {
+                cache = new Cache(cacheDirectory, CACHE_SIZE);
+            }
+
+            if (cache != null) {
+                okHttpClient = new OkHttpClient.Builder()
+                        .cache(cache)
+                        .build();
+            } else {
+                okHttpClient = new OkHttpClient();
+            }
         }
 
         return okHttpClient;
@@ -40,5 +57,4 @@ public class CanteenProxyImpl implements CanteenProxy {
                 .newCall(request)
                 .enqueue(requestParser);
     }
-
 }
