5518df494275954b5a9b269f7529d8a80ef8e6f2
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2018-04-23 15:27:23 +0200

    Refactor with setUp and tearDown method usages

--- app/src/test/java/de/dotwee/rgb/canteen/model/proxy/MensaProxyImplTest.java
+++ app/src/test/java/de/dotwee/rgb/canteen/model/proxy/MensaProxyImplTest.java
@@ -23,29 +23,27 @@ import static org.junit.Assert.assertEquals;
  * Created by lukas on 18.04.18.
  */
 public class MensaProxyImplTest {
+    private final String responsePath = String.format(CanteenProxy.Companion.getURL_PATH(), Location.UNIVERSITY, 16);
+    private final String responseBody = "datum;tag;warengruppe;name;kennz;preis;stud;bed;gast\n" +
+            "16.04.2018;Mo;Suppe;Feine Kräutersuppe (3,A,G,I,AA);V;0,70 / 0,90 / 1,40;0,70;0,90;1,40\n" +
+            "16.04.2018;Mo;HG1;Fusilli mit Zucchini (3,A,C,G,I,AA);V;2,00 / 2,80 / 3,60;2,00;2,80;3,60\n" +
+            "16.04.2018;Mo;HG2;Schweinegulasch mit Bio-Fusilli (3,A,AA);S;2,80 / 3,40 / 4,40;2,80;3,40;4,40\n" +
+            "16.04.2018;Mo;HG3;Hähnchen-Filet-Spieß mit Reis (3,G,I);G;3,60 / 4,60 / 5,10;3,60;4,60;5,10\n" +
+            "16.04.2018;Mo;B1;Kartoffelpüree (3,G);V;0,70 / 0,90 / 1,40;0,70;0,90;1,40\n" +
+            "16.04.2018;Mo;B2;Paprikagemüse ungarisch (3,I);V;0,80 / 1,00 / 1,50;0,80;1,00;1,50\n" +
+            "16.04.2018;Mo;B3;Bio-Basmatireis;B,VG;0,80 / 1,00 / 1,50;0,80;1,00;1,50";
+    private MockWebServer server;
+
     @Before
     public void setUp() throws Exception {
-    }
-
-    @After
-    public void tearDown() throws Exception {
+        server = new MockWebServer();
+        server.enqueue(new MockResponse().setBody(responseBody));
+        server.start();
     }
 
     @Test
     public void newCall() throws Exception {
-        MockWebServer server = new MockWebServer();
-        server.enqueue(new MockResponse().setBody("datum;tag;warengruppe;name;kennz;preis;stud;bed;gast\n" +
-                "16.04.2018;Mo;Suppe;Feine Kräutersuppe (3,A,G,I,AA);V;0,70 / 0,90 / 1,40;0,70;0,90;1,40\n" +
-                "16.04.2018;Mo;HG1;Fusilli mit Zucchini (3,A,C,G,I,AA);V;2,00 / 2,80 / 3,60;2,00;2,80;3,60\n" +
-                "16.04.2018;Mo;HG2;Schweinegulasch mit Bio-Fusilli (3,A,AA);S;2,80 / 3,40 / 4,40;2,80;3,40;4,40\n" +
-                "16.04.2018;Mo;HG3;Hähnchen-Filet-Spieß mit Reis (3,G,I);G;3,60 / 4,60 / 5,10;3,60;4,60;5,10\n" +
-                "16.04.2018;Mo;B1;Kartoffelpüree (3,G);V;0,70 / 0,90 / 1,40;0,70;0,90;1,40\n" +
-                "16.04.2018;Mo;B2;Paprikagemüse ungarisch (3,I);V;0,80 / 1,00 / 1,50;0,80;1,00;1,50\n" +
-                "16.04.2018;Mo;B3;Bio-Basmatireis;B,VG;0,80 / 1,00 / 1,50;0,80;1,00;1,50"));
-        server.start();
-
-        String urlPath = String.format(CanteenProxy.Companion.getURL_PATH(), Location.UNIVERSITY, 16);
-        HttpUrl httpUrl = server.url(urlPath);
+        HttpUrl httpUrl = server.url(responsePath);
         MensaProxyImpl canteenProxy = new MensaProxyImpl(null);
 
         CanteenCallback canteenCallback = new CanteenCallback() {
@@ -65,9 +63,11 @@ public class MensaProxyImplTest {
         canteenProxy.newCall(requestParser, httpUrl);
 
         RecordedRequest request = server.takeRequest();
-        assertEquals(urlPath, request.getPath());
-
-        server.shutdown();
+        assertEquals(responsePath, request.getPath());
     }
 
+    @After
+    public void tearDown() throws Exception {
+        server.shutdown();
+    }
 }
