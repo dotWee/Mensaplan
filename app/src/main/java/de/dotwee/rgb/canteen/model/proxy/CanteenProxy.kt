ba179c6f56c3a9d00421ac834ac1738003838e43
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2018-04-23 12:33:15 +0200

    Rewrite CanteenProxy as Kotlin interface with companion object

---
+++ app/src/main/java/de/dotwee/rgb/canteen/model/proxy/CanteenProxy.kt
@@ -0,0 +1,24 @@
+package de.dotwee.rgb.canteen.model.proxy
+
+import de.dotwee.rgb.canteen.model.Location
+import okhttp3.HttpUrl
+import okhttp3.OkHttpClient
+
+/**
+ * Created by lukas on 18.04.18.
+ */
+
+interface CanteenProxy {
+
+    val httpClient: OkHttpClient
+
+    fun getHttpUrl(location: Location, weeknumber: Int): HttpUrl
+
+    fun newCall(requestParser: RequestParser, httpUrl: HttpUrl)
+
+    companion object {
+        val URL_BASE = "http://www.stwno.de"
+        val URL_PATH = "/infomax/daten-extern/csv/%s/%s.csv"
+        val URL_FORMAT = URL_BASE + URL_PATH
+    }
+}
