c9c3e7e6b2c4825af8265d949b3046ef5791cf15
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2018-04-24 14:35:01 +0200

    First definition of the scrollable location tab layout

---
+++ app/src/main/java/de/dotwee/rgb/canteen/Constants.java
@@ -0,0 +1,8 @@
+package de.dotwee.rgb.canteen;
+
+import de.dotwee.rgb.canteen.model.Price;
+
+public class Constants {
+
+    public static Price DEFAUTL_PRICE = Price.ALL;
+}
---
+++ app/src/main/java/de/dotwee/rgb/canteen/view/custom/LocationTabLayout.java
@@ -0,0 +1,54 @@
+package de.dotwee.rgb.canteen.view.custom;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TabLayout;
+
+import de.dotwee.rgb.canteen.model.Location;
+
+public class LocationTabLayout extends TabLayout implements TabLayout.OnTabSelectedListener {
+    Callback callback;
+
+    public LocationTabLayout(Context context) {
+        super(context);
+
+        for (Location location : Location.values()) {
+            Tab tab = newTab();
+
+            tab.setText(location.getNameVal());
+            tab.setTag(location);
+            addTab(tab);
+        }
+
+        addOnTabSelectedListener(this);
+    }
+
+    public void setCallback(Callback callback) {
+        this.callback = callback;
+    }
+
+    @Override
+    public void onTabSelected(Tab tab) {
+        if (callback != null) {
+            Location location = (Location) tab.getTag();
+            assert location != null;
+
+            callback.onLocationSelected(location);
+        }
+    }
+
+    @Override
+    public void onTabUnselected(Tab tab) {
+
+    }
+
+    @Override
+    public void onTabReselected(Tab tab) {
+        onTabSelected(tab);
+    }
+
+    public interface Callback {
+
+        void onLocationSelected(@NonNull Location location);
+    }
+}
